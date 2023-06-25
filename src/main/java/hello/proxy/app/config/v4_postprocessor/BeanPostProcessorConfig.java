c51a54adb3ec85cc19be8310ec2f4bbaef4f75d5
bluepeng9 <bluepeng9@gmail.com>
2023-06-25 21:08:01 +0900

    feat: Add PackageLogTracePostProcessor

---
+++ src/main/java/hello/proxy/app/config/v4_postprocessor/BeanPostProcessorConfig.java
@@ -0,0 +1,34 @@
+package hello.proxy.app.config.v4_postprocessor;
+
+import hello.proxy.app.config.AppV1Config;
+import hello.proxy.app.config.AppV2Config;
+import hello.proxy.app.config.v3_proxyfactory.LogTraceAdvice;
+import hello.proxy.app.config.v4_postprocessor.postprocessor.PackageLogTracePostProcessor;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class BeanPostProcessorConfig {
+
+    @Bean
+    public PackageLogTracePostProcessor logTracePostProcessor(LogTrace logTrace) {
+        return new PackageLogTracePostProcessor("hello.proxy.app", getAdvisor(logTrace));
+    }
+
+    private Advisor getAdvisor(LogTrace logTrace) {
+        //pointCut
+        NameMatchMethodPointcut pointCut = new NameMatchMethodPointcut();
+        pointCut.setMappedNames("request*", "order*", "save*");
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointCut, advice);
+    }
+}
